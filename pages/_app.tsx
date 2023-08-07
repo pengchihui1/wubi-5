6836c4bbd45d9616e5527e7c19a7983c5b061a99
pengchihui1 <51830726+pengchihui1@users.noreply.github.com>
2023-08-07 11:37:21 +0800

    基础项目结构

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import type {AppProps} from 'next/app'
+
+import '@/styles/globals.css'
+
+export default function App({Component, pageProps}: AppProps) {
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 pt-32`}
+    >
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          五笔练习
+        </div>
+      </div>
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+      </div>
+    </main>
+  )
+}
